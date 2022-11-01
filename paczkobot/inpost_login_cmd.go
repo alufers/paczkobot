cb6b5df6917af96e03bec3cbc6cc656b7d3c7a83
alufers <alufers@wp.pl>
2022-11-01 16:04:41 +0100

    fixup duplicate inpost credentials

--- paczkobot/inpost_login_cmd.go
+++ paczkobot/inpost_login_cmd.go
@@ -3,6 +3,8 @@ package paczkobot
 import (
 	"context"
 	"fmt"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
 type InpostLoginCommand struct {
@@ -39,11 +41,17 @@ func (f *InpostLoginCommand) Execute(ctx context.Context, args *CommandArguments
 	if err != nil {
 		return err
 	}
+
 	creds, err := f.App.InpostService.ConfirmSMSCode(phoneNumber, code)
 	if err != nil {
 		return fmt.Errorf("failed to confirm sms code: %v", err)
 	}
+
 	creds.TelegramUserID = args.FromUserID
+	err = f.App.DB.Where("telegram_user_id = ? AND phone_number = ?", args.FromUserID, phoneNumber).FirstOrCreate(&creds).Error
+	if err != nil {
+		return fmt.Errorf("failed to delete existing credentials: %v", err)
+	}
 	if err := f.App.DB.Save(creds).Error; err != nil {
 		return fmt.Errorf("failed to save credentials: %v", err)
 	}
@@ -52,6 +60,10 @@ func (f *InpostLoginCommand) Execute(ctx context.Context, args *CommandArguments
 	)
 	if err != nil {
 		return fmt.Errorf("failed to scan user inpost packages: %v", err)
+	}
+	msg := tgbotapi.NewMessage(args.ChatID, fmt.Sprintf("Successfully logged in to Inpost with phone number %s", phoneNumber))
+	if _, err := f.App.Bot.Send(msg); err != nil {
+		return fmt.Errorf("failed to send message: %v", err)
 	}
 	return nil
 }
