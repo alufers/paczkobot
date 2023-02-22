407483868ba343464b30a7aa92bd87aedb96b428
alufers <alufers@wp.pl>
2023-02-22 15:42:06 +0100

    chore: update UA

--- commondata/common_headers.go
+++ commondata/common_headers.go
@@ -4,7 +4,7 @@ import "net/http"
 
 func SetCommonHTTPHeaders(header *http.Header) {
 
-	header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36")
+	header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36")
 	header.Set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8")
 	header.Set("Accept-Language", "en-US,en;q=0.5")
 	header.Set("Upgrade-Insecure-Requests", "1")
