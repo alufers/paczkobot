795125a45fe3154b0cb9d65f5decbbf78729a18d
alufers <alufers@wp.pl>
2023-09-21 17:28:41 +0200

    Add FetchGoqueryDocument for generic goquery fetching

---
+++ provider_util/fetch_goquery_document.go
@@ -0,0 +1,37 @@
+package providerutil
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	"github.com/PuerkitoBio/goquery"
+	"github.com/alufers/paczkobot/commondata"
+	"github.com/alufers/paczkobot/commonerrors"
+)
+
+func FetchGoqueryDocument(ctx context.Context, providerName string, url string, checkStatusCode bool) (*goquery.Document, error) {
+	req, err := http.NewRequestWithContext(ctx, "GET", url, nil)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create GET request: %w", err)
+	}
+	commondata.SetCommonHTTPHeaders(&req.Header)
+
+	httpResponse, err := http.DefaultClient.Do(req)
+	if err != nil {
+		// Assuming NewNetworkError is available in your package
+		return nil, commonerrors.NewNetworkError(providerName, req)
+	}
+	defer httpResponse.Body.Close()
+
+	if checkStatusCode && httpResponse.StatusCode != 200 {
+		return nil, fmt.Errorf("HTTP status code %v", httpResponse.StatusCode)
+	}
+
+	doc, err := goquery.NewDocumentFromReader(httpResponse.Body)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read HTML response: %w", err)
+	}
+
+	return doc, nil
+}
