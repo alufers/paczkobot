eb8656255b86c45937bdc0d25724eec9e1ee4705
alufers <alufers@wp.pl>
2023-10-08 12:36:42 +0200

    Fix command dispatcher not running hooks, and add test

--- tghelpers/command_dispatcher.go
+++ tghelpers/command_dispatcher.go
@@ -44,96 +44,99 @@ func (d *CommandDispatcher) RunUpdateLoop() error {
 	ctx := context.Background()
 	for u := range updates {
 		// run command processing asynchronously
-		go func(u tgbotapi.Update) {
-			var cmdText string
-			args := &CommandArguments{
-				AskService:     d.AskService,
-				Update:         &u,
-				NamedArguments: map[string]string{},
-			}
-			if u.Message != nil {
-				cmdText = u.Message.Text
-				args.ChatID = u.Message.Chat.ID
-				args.FromUserID = u.Message.From.ID
-			}
-			if u.CallbackQuery != nil {
-				cmdText = u.CallbackQuery.Data
-				args.ChatID = u.CallbackQuery.Message.Chat.ID
-				args.FromUserID = u.CallbackQuery.From.ID
-			}
-			seg := strings.Split(cmdText, " ")
-			args.CommandName = seg[0]
-			args.Arguments = seg[1:]
-
-			ctx := context.WithValue(ctx, UpdateContextKey, u)
-			ctx = context.WithValue(ctx, ArgsContextKey, args)
-
-			// TODO: move this to a nice hook
-			if strings.HasSuffix(args.CommandName, "@har") {
-				ctx = httphelpers.WithHarLoggerStorage(ctx)
-				for _, hook := range d.UpdateHooks {
-					if hook.OnUpdate(ctx) {
-						return // hook has handled the message stop processing
-					}
-				}
-			}
-
-			var err error
-
-			for _, cmd := range d.Commands {
-				if CommandMatches(cmd, cmdText) {
-					args.Command = cmd
-					for i, argTpl := range cmd.Arguments() {
-						if argTpl.Variadic {
-							args.NamedArguments[argTpl.Name] = strings.Join(args.Arguments[i:], " ")
-							break
-						}
-						if i >= len(args.Arguments) {
-							break
-						}
-						args.NamedArguments[argTpl.Name] = args.Arguments[i]
-					}
-
-					err = cmd.Execute(ctx)
-
-					break
-				}
-			}
-
-			storage := httphelpers.GetHarLoggerStorage(ctx)
-
-			if storage != nil {
-
-				jsonData, err := storage.GetJSONData()
-				if err != nil {
-					log.Printf("Error while getting HAR data: %v", err)
-				} else {
-					sendDoc := tgbotapi.NewDocument(args.ChatID, tgbotapi.FileReader{
-						Name:   "har.json",
-						Reader: strings.NewReader(string(jsonData)),
-					})
-					sendDoc.Caption = "HAR data"
-					_, err := d.BotAPI.Send(sendDoc)
-					if err != nil {
-						log.Printf("Error while sending HAR data: %v", err)
-					}
-				}
-			}
-			if err != nil {
-				log.Printf("Error while processing command %v: %v", cmdText, err)
-				msg := tgbotapi.NewMessage(args.ChatID, "🚫 Error: <b>"+html.EscapeString(err.Error())+"</b>")
-				msg.ParseMode = "HTML"
-				if u.Message != nil {
-					msg.ReplyToMessageID = u.Message.MessageID
-				}
-				_, err := d.BotAPI.Send(msg)
-				if err != nil {
-					log.Printf("An error has occurred while sending an error message: %v", err)
-				}
-			}
-		}(u)
+		go d.processIncomingUpdate(ctx, u)
 	}
 	return nil
+}
+
+func (d *CommandDispatcher) processIncomingUpdate(ctx context.Context, u tgbotapi.Update) {
+	var cmdText string
+	args := &CommandArguments{
+		AskService:     d.AskService,
+		Update:         &u,
+		NamedArguments: map[string]string{},
+	}
+	if u.Message != nil {
+		cmdText = u.Message.Text
+		args.ChatID = u.Message.Chat.ID
+		args.FromUserID = u.Message.From.ID
+	}
+	if u.CallbackQuery != nil {
+		cmdText = u.CallbackQuery.Data
+		args.ChatID = u.CallbackQuery.Message.Chat.ID
+		args.FromUserID = u.CallbackQuery.From.ID
+	}
+	seg := strings.Split(cmdText, " ")
+	args.CommandName = seg[0]
+	args.Arguments = seg[1:]
+
+	ctx = context.WithValue(ctx, UpdateContextKey, u)
+	ctx = context.WithValue(ctx, ArgsContextKey, args)
+
+	// TODO: move this to a nice hook
+	if strings.HasSuffix(args.CommandName, "@har") {
+		ctx = httphelpers.WithHarLoggerStorage(ctx)
+	}
+
+	for _, hook := range d.UpdateHooks {
+		if hook.OnUpdate(ctx) {
+			return // hook has handled the message stop processing
+		}
+	}
+
+	var err error
+
+	for _, cmd := range d.Commands {
+		if CommandMatches(cmd, cmdText) {
+			args.Command = cmd
+			for i, argTpl := range cmd.Arguments() {
+				if argTpl.Variadic {
+					args.NamedArguments[argTpl.Name] = strings.Join(args.Arguments[i:], " ")
+					break
+				}
+				if i >= len(args.Arguments) {
+					break
+				}
+				args.NamedArguments[argTpl.Name] = args.Arguments[i]
+			}
+
+			err = cmd.Execute(ctx)
+
+			break
+		}
+	}
+
+	storage := httphelpers.GetHarLoggerStorage(ctx)
+
+	if storage != nil {
+
+		jsonData, err := storage.GetJSONData()
+		if err != nil {
+			log.Printf("Error while getting HAR data: %v", err)
+		} else {
+			sendDoc := tgbotapi.NewDocument(args.ChatID, tgbotapi.FileReader{
+				Name:   "har.json",
+				Reader: strings.NewReader(string(jsonData)),
+			})
+			sendDoc.Caption = "HAR data"
+			_, err := d.BotAPI.Send(sendDoc)
+			if err != nil {
+				log.Printf("Error while sending HAR data: %v", err)
+			}
+		}
+	}
+	if err != nil {
+		log.Printf("Error while processing command %v: %v", cmdText, err)
+		msg := tgbotapi.NewMessage(args.ChatID, "🚫 Error: <b>"+html.EscapeString(err.Error())+"</b>")
+		msg.ParseMode = "HTML"
+		if u.Message != nil {
+			msg.ReplyToMessageID = u.Message.MessageID
+		}
+		_, err := d.BotAPI.Send(msg)
+		if err != nil {
+			log.Printf("An error has occurred while sending an error message: %v", err)
+		}
+	}
 }
 
 // RequestSetMyCommands registers the commands in the Telegram bot API.
---
+++ tghelpers/command_dispatcher_test.go
@@ -0,0 +1,45 @@
+package tghelpers
+
+import (
+	"context"
+	"testing"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+type FakeUpdateHook struct {
+	didRun bool
+}
+
+func (h *FakeUpdateHook) OnUpdate(context.Context) bool {
+	h.didRun = true
+	return true
+}
+
+// a test that chcks if command dispatcher executes update hooks
+func TestCommandDispatcherUpdateHooks(t *testing.T) {
+	botAPI := &MockBotApi{}
+	// create a fake command dispatcher
+	dispatcher := NewCommandDispatcher(botAPI, nil)
+
+	fh := &FakeUpdateHook{}
+	// register a fake update hook
+	dispatcher.RegisterUpdateHooks(fh)
+
+	dispatcher.processIncomingUpdate(context.Background(), tgbotapi.Update{
+		Message: &tgbotapi.Message{
+			Text: "test",
+			Chat: &tgbotapi.Chat{
+				ID: 123,
+			},
+			From: &tgbotapi.User{
+				ID: 456,
+			},
+		},
+	})
+
+	// check if the hook was run
+	if !fh.didRun {
+		t.Fatal("update hook was not run")
+	}
+}
